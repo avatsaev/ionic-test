abe8fcd6e900aecc618a67939d8c45c1b0116ac3
A. Vatsaev <azero79@gmail.com>
2016-02-12 11:07:43 +0100

    gulped

---
+++ .tmp/scripts/controllers/mainController.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name IonicTest.controller:MainController
+ * @description
+ * # MainController
+ */
+angular.module('IonicTest')
+  .controller('MainController', function($scope) {
+
+    // do something with $scope
+
+  });
