ed960f4d0549c3d0f45d8aee1c19cde47d754db8
A. Vatsaev <azero79@gmail.com>
2016-02-12 11:02:52 +0100

    init

---
+++ app/scripts/controllers/settingsController.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name IonicTest.controller:SettingsController
+ * @description
+ * # SettingsController
+ */
+angular.module('IonicTest')
+  .controller('SettingsController', function($scope) {
+
+    // do something with $scope
+
+  });
